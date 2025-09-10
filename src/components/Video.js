5a82947242e5c4523741b1a1bbed5d50c82e1d42
SrAnarquia <jonarsz@hotmail.com>
2025-09-09 23:43:30 -0600

    Video agregado

--- src/components/Video.js
+++ src/components/Video.js
@@ -7,7 +7,7 @@ export default function Video() {
         <iframe
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/VIDEO_ID"
+          src="https://www.youtube.com/embed/Ung1lMjrzko"
           title="Video de Jonathan Corpus"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
