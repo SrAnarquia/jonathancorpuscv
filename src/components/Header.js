faa26fa5d8a4ae50d976ae04320e8f8814839e58
SrAnarquia <jonarsz@hotmail.com>
2026-03-16 22:17:07 -0600

    Cambios 3162026 CV EspEng

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,18 +4,41 @@ export default function Header() {
   return (
     <header>
       <h1>Jonathan Corpus</h1>
-      <p className="subtitle">Developer | Electronics & Automation Engineer | Systems Administrator </p>
-      <div class="contact">
-      	<a href="mailto:Jonathan.Ecorpus@gmail.com">Jonathan.Ecorpus@gmail.com</a> | 
-      	<a href="http://www.linkedin.com/in/jonathan-corpus-462067237" target="_blank">LinkedIn</a>
+      <p className="subtitle">
+        Developer | Electronics & Automation Engineer | Systems Administrator
+      </p>
+
+      <div className="contact">
+        <a href="mailto:Jonathan.Ecorpus@gmail.com">
+          Jonathan.Ecorpus@gmail.com
+        </a>{" "}
+        |{" "}
+        <a
+          href="http://www.linkedin.com/in/jonathan-corpus-462067237"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
       </div>
+
       <nav>
         <Link to="/">Proyectos</Link>
-        <Link to="/sobremi">Sobre mí </Link>
+        <Link to="/sobremi">Sobre mí</Link>
         <Link to="/video">Video</Link>
-	<a class="" href="/JonathanCorpusCV.pdf" download>
-  		Mi CV
-	</a>
+
+        <div className="cv-dropdown">
+          <button className="cv-button">Mi CV ▾</button>
+
+          <div className="cv-menu">
+            <a href="/JonathanCorpusCV_ESP.pdf" download>
+              CV Español
+            </a>
+            <a href="/JonathanCorpusCV_ENG.pdf" download>
+              CV English
+            </a>
+          </div>
+        </div>
       </nav>
     </header>
   );
